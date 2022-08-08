1234591c8e80f655bbea8001bc9e2dcb31abbb20
Markus Wippler <markus.wippler@uibk.ac.at>
2022-08-08 16:00:45 +0200

    Refactor exception types to be closer together

--- include/libenvpp.hpp
+++ include/libenvpp.hpp
@@ -16,11 +16,6 @@ namespace env {
 
 class prefix;
 
-class duplicate_option : public std::invalid_argument {
-  public:
-	duplicate_option() : std::invalid_argument("Same option specified more than once") {}
-};
-
 namespace detail {
 
 class empty_base {};
@@ -94,6 +89,23 @@ class var_base {
 
 } // namespace detail
 
+class duplicate_option : public std::invalid_argument {
+  public:
+	duplicate_option() : std::invalid_argument("Same option specified more than once") {}
+};
+
+class parser_error : public std::runtime_error {
+  public:
+	parser_error() = delete;
+	parser_error(const std::string_view message) : std::runtime_error(std::string(message)) {}
+};
+
+class unrecognized_option : public std::runtime_error {
+  public:
+	unrecognized_option() = delete;
+	unrecognized_option(const std::string_view message) : std::runtime_error(std::string(message)) {}
+};
+
 template <typename T>
 class option : public detail::var_base<T, option> {
   public:
@@ -144,18 +156,6 @@ class var : public detail::var_base<T, var>,
 	{}
 
 	friend prefix;
-};
-
-class parser_error : public std::runtime_error {
-  public:
-	parser_error() = delete;
-	parser_error(const std::string_view message) : std::runtime_error(std::string(message)) {}
-};
-
-class unrecognized_option : public std::runtime_error {
-  public:
-	unrecognized_option() = delete;
-	unrecognized_option(const std::string_view message) : std::runtime_error(std::string(message)) {}
 };
 
 class validation_result {
