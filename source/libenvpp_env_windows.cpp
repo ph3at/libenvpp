6d60298a6730fb52446aeca948d59f17ac1969dd
BlackMark <me@blackmark.me>
2022-08-19 09:38:00 +0200

    Change windows environment functions to always use unicode

--- source/libenvpp_env_windows.cpp
+++ source/libenvpp_env_windows.cpp
@@ -5,7 +5,6 @@
 #define NOMINMAX
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-#include <tchar.h>
 
 #include <array>
 #include <cwchar>
@@ -14,8 +13,6 @@
 #include <libenvpp_check.hpp>
 
 namespace env::detail {
-
-using tstring = std::conditional_t<std::is_same_v<TCHAR, char>, std::string, std::wstring>;
 
 static inline std::string convert_string(const std::wstring& str)
 {
@@ -37,49 +34,31 @@ static inline std::wstring convert_string(const std::string& str)
 	return dst;
 }
 
-static inline std::string from_tstring(const tstring& str)
-{
-	if constexpr (std::is_same_v<tstring, std::string>) {
-		return str;
-	} else {
-		return convert_string(str);
-	}
-}
-
-static inline tstring to_tstring(const std::string& str)
-{
-	if constexpr (std::is_same_v<tstring, std::string>) {
-		return str;
-	} else {
-		return convert_string(str);
-	}
-}
-
 std::unordered_map<std::string, std::string> get_environment()
 {
 	auto env_map = std::unordered_map<std::string, std::string>{};
 
-	const auto environment = GetEnvironmentStrings();
+	const auto environment = GetEnvironmentStringsW();
 	if (!environment) {
 		return env_map;
 	}
 
 	for (const auto* var = environment; *var; ++var) {
-		auto var_name_value = std::array<tstring, 2>{};
+		auto var_name_value = std::array<std::wstring, 2>{};
 		auto idx = std::size_t{0};
 		for (; *var; ++var) {
-			if (idx == 0 && *var == _T('=')) {
+			if (idx == 0 && *var == L'=') {
 				++idx;
 			} else {
 				var_name_value[idx] += *var;
 			}
 		}
 		if (!var_name_value[0].empty()) {
-			env_map[from_tstring(var_name_value[0])] = from_tstring(var_name_value[1]);
+			env_map[convert_string(var_name_value[0])] = convert_string(var_name_value[1]);
 		}
 	}
 
-	[[maybe_unused]] const auto env_strings_were_freed = FreeEnvironmentStrings(environment);
+	[[maybe_unused]] const auto env_strings_were_freed = FreeEnvironmentStringsW(environment);
 	LIBENVPP_CHECK(env_strings_were_freed);
 
 	return env_map;
@@ -87,29 +66,29 @@ std::unordered_map<std::string, std::string> get_environment()
 
 std::optional<std::string> get_environment_variable(const std::string_view name)
 {
-	const auto var_name = to_tstring(std::string(name));
-	const auto buffer_size = GetEnvironmentVariable(var_name.c_str(), nullptr, 0);
+	const auto var_name = convert_string(std::string(name));
+	const auto buffer_size = GetEnvironmentVariableW(var_name.c_str(), nullptr, 0);
 	if (buffer_size == 0) {
 		return {};
 	}
 	// -1 because std::string already contains implicit null terminator
-	auto value = tstring(buffer_size - 1, _T('\0'));
-	const auto env_var_got = GetEnvironmentVariable(var_name.c_str(), value.data(), buffer_size);
+	auto value = std::wstring(buffer_size - 1, L'\0');
+	const auto env_var_got = GetEnvironmentVariableW(var_name.c_str(), value.data(), buffer_size);
 	LIBENVPP_CHECK(env_var_got != 0);
-	return from_tstring(value);
+	return convert_string(value);
 }
 
 void set_environment_variable(const std::string_view name, const std::string_view value)
 {
 	[[maybe_unused]] const auto env_var_was_set =
-	    SetEnvironmentVariable(to_tstring(std::string(name)).c_str(), to_tstring(std::string(value)).c_str());
+	    SetEnvironmentVariableW(convert_string(std::string(name)).c_str(), convert_string(std::string(value)).c_str());
 	LIBENVPP_CHECK(env_var_was_set);
 }
 
 void delete_environment_variable(const std::string_view name)
 {
 	[[maybe_unused]] const auto env_var_was_deleted =
-	    SetEnvironmentVariable(to_tstring(std::string(name)).c_str(), nullptr);
+	    SetEnvironmentVariableW(convert_string(std::string(name)).c_str(), nullptr);
 	LIBENVPP_CHECK(env_var_was_deleted);
 }
 
