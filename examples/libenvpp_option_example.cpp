053fda74e129a8c74ca37acbdbd6b64e4dd4adc6
Markus Wippler <markus.wippler@uibk.ac.at>
2022-09-12 13:53:54 +0200

    Add option usage example

---
+++ examples/libenvpp_option_example.cpp
@@ -0,0 +1,66 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+#include <libenvpp.hpp>
+
+enum class option {
+	first_choice,
+	second_choice,
+	third_choice,
+};
+
+namespace env {
+template <>
+struct default_parser<option> {
+	option operator()(const std::string_view str) const
+	{
+		auto opt = option{};
+
+		if (str == "first_choice") {
+			opt = option::first_choice;
+		} else if (str == "second_choice") {
+			opt = option::second_choice;
+		} else if (str == "third_choice") {
+			opt = option::third_choice;
+		} else {
+			throw parser_error{std::string("Unable to parse '") + std::string(str) + "'"};
+		}
+
+		return opt;
+	}
+};
+} // namespace env
+
+int main()
+{
+	auto pre = env::prefix("OPTION");
+
+	const auto option_id = pre.register_required_option<option>(
+	    "CHOICE", {option::first_choice, option::second_choice, option::third_choice});
+
+	const auto parsed_and_validated_pre = pre.parse_and_validate();
+
+	if (parsed_and_validated_pre.ok()) {
+		const auto opt = parsed_and_validated_pre.get(option_id);
+
+		std::cout << "Chosen: ";
+		switch (opt) {
+		case option::first_choice:
+			std::cout << "first_choice" << std::endl;
+			break;
+		case option::second_choice:
+			std::cout << "second_choice" << std::endl;
+			break;
+		case option::third_choice:
+			std::cout << "third_choice" << std::endl;
+			break;
+		}
+	} else {
+		std::cout << parsed_and_validated_pre.warning_message() << std::endl;
+		std::cout << parsed_and_validated_pre.error_message() << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
