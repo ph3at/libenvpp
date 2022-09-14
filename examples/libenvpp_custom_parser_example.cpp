f50ba52e660a98889e55649e6f5b74f91f84b465
Markus Wippler <markus.wippler@uibk.ac.at>
2022-09-14 15:39:48 +0200

    Change custom parser example

--- examples/libenvpp_custom_parser_example.cpp
+++ examples/libenvpp_custom_parser_example.cpp
@@ -1,13 +1,26 @@
 #include <cstdlib>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <string_view>
+#include <vector>
 
 #include <libenvpp.hpp>
 
+static std::vector<std::string> split(const std::string_view str, const char delimiter = ',')
+{
+	auto result = std::vector<std::string>{};
+	auto sstream = std::istringstream(std::string(str));
+	auto item = std::string{};
+	while (std::getline(sstream, item, delimiter)) {
+		result.push_back(std::move(item));
+	}
+	return result;
+}
+
 struct program_data {
-	std::string as_string;
-	int as_number;
+	int number;
+	float percent;
 };
 
 namespace env {
@@ -15,9 +28,18 @@ template <>
 struct default_parser<program_data> {
 	program_data operator()(const std::string_view str) const
 	{
+		const auto split_str = split(str);
+		if (split_str.size() != 2) {
+			// Report an error if the input does not have the expected format
+			throw parser_error{"Expected 2 comma delimited values"};
+		}
+
 		auto parsed_data = program_data{};
-		parsed_data.as_string = default_parser<std::string>{}(str);
-		parsed_data.as_number = default_parser<int>{}(str);
+
+		// Delegate parsing of primitive types to the default_parser
+		parsed_data.number = default_parser<int>{}(split_str[0]);
+		parsed_data.percent = default_parser<float>{}(split_str[1]);
+
 		return parsed_data;
 	}
 };
@@ -34,8 +56,8 @@ int main()
 	if (parsed_and_validated_pre.ok()) {
 		const auto prog_data = parsed_and_validated_pre.get(program_data_id);
 
-		std::cout << "Program data as string: " << prog_data.as_string << std::endl;
-		std::cout << "Program data as number: " << prog_data.as_number << std::endl;
+		std::cout << "Program data number : " << prog_data.number << std::endl;
+		std::cout << "Program data percent: " << prog_data.percent << std::endl;
 	} else {
 		std::cout << parsed_and_validated_pre.warning_message() << std::endl;
 		std::cout << parsed_and_validated_pre.error_message() << std::endl;
