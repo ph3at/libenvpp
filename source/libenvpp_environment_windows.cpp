aab933e5d14db1d1071e3367081b625f89419dd0
BlackMark <me@blackmark.me>
2024-09-18 15:33:20 +0200

    Fix macro redefinition warning when compiling with mingw

--- source/libenvpp_environment_windows.cpp
+++ source/libenvpp_environment_windows.cpp
@@ -2,8 +2,12 @@
 
 #include <libenvpp/detail/environment.hpp>
 
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
+#ifndef WIN32_LEAN_AND_MEAN
 #define WIN32_LEAN_AND_MEAN
+#endif
 #include <windows.h>
 
 #include <array>
